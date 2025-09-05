4facc849bbef0f63fe61bad9d1c2c5469d2a7df7
Jeff Martinez <jeffmartinez@isufst.edu.ph>
2025-09-06 07:28:36 +0800

    Fix: Added env file to scure API

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import React from "react";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCh5P10H-KbP_TN28l722SFwixNuLlilkI",
-  authDomain: "attendance-tracker-app-804fe.firebaseapp.com",
-  projectId: "attendance-tracker-app-804fe",
-  storageBucket: "attendance-tracker-app-804fe.firebasestorage.app",
-  messagingSenderId: "44754632357",
-  appId: "1:44754632357:web:54460b66884ac1d5d1574c",
-  measurementId: "G-EH9E8BXQYC"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 
